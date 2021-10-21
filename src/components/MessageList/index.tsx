f8e7e890c33e37e4c5d96b850192025a6b275a59
Marco Nemetala <marcoantonionemetala@gmail.com>
2021-10-21 18:55:44 -0300

    Conexão do front com back (Mensagens)

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,49 +1,47 @@
 import sytles from "./styles.module.scss";
 import logoImg from "../../assets/logo.svg";
+import { api } from "../../services/api";
+import { useEffect, useState } from "react";
+
+// Tipagem da mensagem em Typescript
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
 
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>("messages/last3").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
   return (
     <div className={sytles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile2021" />
       <ul className={sytles.messageList}>
-        <li className={sytles.message}>
-          <p className={sytles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser melhor
-            do que do ano passado ✔{" "}
-          </p>
-          <div className={sytles.messageUser}>
-            <div className={sytles.userImage}>
-              <img src="https://github.com/Hadeshade.png" alt="Marco Antonio" />
-            </div>
-            <span>Marco Antonio</span>
-          </div>
-        </li>
-
-        <li className={sytles.message}>
-          <p className={sytles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser melhor
-            do que do ano passado ✔{" "}
-          </p>
-          <div className={sytles.messageUser}>
-            <div className={sytles.userImage}>
-              <img src="https://github.com/Hadeshade.png" alt="Marco Antonio" />
-            </div>
-            <span>Marco Antonio</span>
-          </div>
-        </li>
-
-        <li className={sytles.message}>
-          <p className={sytles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser melhor
-            do que do ano passado ✔{" "}
-          </p>
-          <div className={sytles.messageUser}>
-            <div className={sytles.userImage}>
-              <img src="https://github.com/Hadeshade.png" alt="Marco Antonio" />
-            </div>
-            <span>Marco Antonio</span>
-          </div>
-        </li>
+        {messages.map((message) => {
+          return (
+            <li key={message.id} className={sytles.message}>
+              <p className={sytles.messageContent}>{message.text}</p>
+              <div className={sytles.messageUser}>
+                <div className={sytles.userImage}>
+                  <img
+                    src="https://github.com/Hadeshade.png"
+                    alt={message.user.name}
+                  />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
